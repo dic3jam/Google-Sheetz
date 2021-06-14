44a3d747bb3689b4bfe8161c5fcdf8e5fcb6e05a
dic3jam <dic3jam@gmail.com>
2021-06-14 15:43:50 -0400

    swatting them bugs

--- entangle.js
+++ entangle.js
@@ -7,11 +7,22 @@
  * To use entanglement, add a javascript array in a random cell
  * in your google sheet. This array must be in the following
  * format:
- * [['Cell1','Cell2'],['Cell3','Cell4']]
+ * Cell1,Cell2 Cell3,Cell4 Cell5,Cell6
+ * So sets of cells to entangle separated by a space, 
+ * Cells to entangle separated by a comma
+ * Seet this variable:*/
+ var entangleArray = "";
+/* to the cell that contains the array
+ * Then, set a trigger for the entangleRunner 
+ * function.
  */
 
-//Add which cell holds the array here:
-var entangleArray = "";
+//For some reason Google Sheets does not like passing
+//in values by reference.... so here are some other globals
+//if entanglement was already initialized, will not run initEntanglementArray()
+var init = false;
+var sheet = SpreadsheetApp.getActiveSpreadsheet();
+var entanglements = []
 
 class Entangle {
   /* constructor 
@@ -24,12 +35,12 @@ class Entangle {
    * @param sheet - the sheet this particular entanglement 
    * set is for
    */
-   constructor(cell1, cell2, sheet) {
-    this.sheet = sheet;
-    this.cell1Pos = cell1.getA1Notation();
-    this.cell2Pos = cell2.getA1Notation();
-    this.cell1Val = cell1.getValue();
-    this.cell2Val = cell2.getValue();
+   constructor(cell1, cell2) {
+    this._sheet = sheet;
+    this.cell1Pos = cell1;
+    this.cell2Pos = cell2;
+    this.cell1Val = this._sheet.getRange(cell1).getValue();
+    this.cell2Val = this._sheet.getRange(cell2).getValue();
     if (this.cell1Val == "" && this.cell2Val == ""){
       this.cell1Val = 0;
       this.cell2Val = 0;
@@ -49,6 +60,7 @@ class Entangle {
    updateCell1() {
       this.cell2Val = this.sheet.getValue(this.cell2Pos);
       this.cell1Val = this.cell2Val;
+      sheet.getRange(this.cell1Pos).setValue(this.cell1Val);
    }
 
   /* function updateCell2
@@ -58,25 +70,44 @@ class Entangle {
    updateCell2() {
       this.cell1Val = this.sheet.getValue(this.cell1Pos);
       this.cell2Val = this.cell1Val;
+      sheet.getRange(this.cell2Pos).setValue(this.cell2Val);
    }
-
-   
 }
 
-//helpers
-function initEntangelementArray(entangleArray) {
-     var en = []
-     foreach(e in entangeArray) 
-      en.append(Entangle(e[0], e[1], SpreadsheetApp.getActiveSpreadsheet())); 
-     return en;
+;
+//main function for running entanglement
+function entangleRunner() {
+  if(!init)
+    entanglements = initEntangelementArray();
+  for(p in entanglements) {
+    if(sheet.getRange(p.cell1Pos).getValue() != p.cell1Val) {
+      p.updateCell2();
+    } else if(sheet.getRange(p.cell2Pos).getValue() != p.cell2Val) {
+      p.updateCell1();
+    } 
+  }
+}
+
+//constructs an array of Entanglements
+function initEntangelementArray() {
+  var en = readArray();
+  var ens = [];
+  for(var i = 0; i < en.length; i++) 
+    ens.push(new Entangle(en[i][0], en[i][1]));
+  init = true;
+  return ens;
 } 
 
-function entangleRunner(entangleArray) {
-
+//pulls in the list of cells that need to be entangled 
+//and returns it as an array
+function readArray() {
+  var i = sheet.getRange(entangleArray).getValue();
+  var i = i.split(" ");
+  for(var j = 0; j < i.length; j++)
+    i[j] = i[j].split(',');
+  return i;
 }
 
-//script
-var en = initEntangelementArray(entangleArray);
-entangleRunner(en);
+
 
 
