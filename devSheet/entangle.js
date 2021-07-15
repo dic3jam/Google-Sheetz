abcbf63bb7785cedfdae258f1434a6728405499c
dic3jam <dic3jam@gmail.com>
2021-07-15 15:02:18 -0400

    soo close on new entangle

--- devSheet/entangle.js
+++ devSheet/entangle.js
@@ -12,10 +12,10 @@
  * So sets of cells to entangle separated by a space, 
  * Cells to entangle separated by a comma
  **************Set this variable:**********************/
- var entangleArray = null;/*[{
-                        set:"",
-                        sheet:""
-                      },
+ var entangleArray = [{
+                        set:"AE8",
+                        sheet:"Blade1"
+                      }]/*,
                       {
                         set:"",
                         sheet:""
@@ -24,79 +24,71 @@
 /* to the cell that contains the array
  */
 
-class Entangle {
-  /* constructor 
-   * Initializes a set of entangled cells 
-   * if value in one but not the other, both set to 
-   * that value else if both already have a value
-   * value is set to cell1, else both set to 0
-   * Can construct with 2 named locations (list being passed it)
-   * or receive a parsed JSON version of entangled cells
-   * returns the Entangle OR null if wrong number of arguments inputted
-   */
-   constructor(sheet, ...args) {
-    if(args.length == 2) {
-        this.entangleList(sheet, args);
-    } else if (args.length == 4) {
-        this.entangleJSON(sheet, args);
-    } else
-        null;
+/***********************************************************************/
+//Classes
+
+class EntangleList {
+
+  constructor(protoList, sheet, offset) {  
+    this.list = []
+    for(var proto of protoList) {
+      var entangle = new Entangle(proto, sheet)
+      this.list.push(entangle);
+    }
+    this.sheet = sheet;
+    this.refRange = offset;
   }
-  entangleList(sheet, args) {
-    this.cell1Pos = args[0];
-    this.cell2Pos = args[1];
-    this.cell1Val = sheet.getRange(args[0]).getValue();
-    this.cell2Val = sheet.getRange(args[1]).getValue();
-    if (this.cell1Val == "" && this.cell2Val == ""){
-        this.cell1Val = 0;
-        this.cell2Val = 0;
-        sheet.getRange(this.cell1Pos).setValue(this.cell1Val);
-        sheet.getRange(this.cell2Pos).setValue(this.cell2Val);
-    } else if (this.cell1Val == "" && this.cell2Val != "") {
-        this.updateCell1(sheet);
-    } else if (this.cell2Val == "" && this.cell1Val != "") {
-        this.updateCell2(sheet);
-    } else {
-        this.updateCell2(sheet);
+
+  checkListValues() {
+    for(var entangle of this.list)
+      entangle.checkValues(this.sheet);
+  }
+
+  toJSON() {
+    return { list: this.list, sheet: this.sheet.getSheetName(), refRange: this.refRange.getA1Notation() }
+  }
+
+}
+
+class Entangle {
+  constructor(proto, sheet, value = 0) {
+    this.list = []
+    for(var i = 0; i < proto.length; i++) {
+      this.list.push(sheet.getRange(proto[i]));
+    } 
+    if(value) {
+        this.value = value;
+        this.setValues(sheet)
+      } else {
+
     }
   }
-  entangleJSON(sheet, args) {
-    this.cell1Pos = args[0];
-    this.cell2Pos = args[1];
-    this.cell1Val = args[2];
-    this.cell2Val = args[3];
-    if (this.cell1Val == "" && this.cell2Val == ""){
-        this.cell1Val = 0;
-        this.cell2Val = 0;
-        sheet.getRange(this.cell1Pos).setValue(this.cell1Val);
-        sheet.getRange(this.cell2Pos).setValue(this.cell2Val);
-    } else if (this.cell1Val == "" && this.cell2Val != "") {
-        this.updateCell1(sheet);
-    } else if (this.cell2Val == "" && this.cell1Val != "") {
-        this.updateCell2(sheet);
-    } /*else {
-        this.updateCell2(sheet);
-    } */
-  }
-  /* function updateCell1
-   * If cell2 has been updated, change cell1
-   * to reflect
-   */
-   updateCell1(sheet) {
-      this.cell2Val = sheet.getRange(this.cell2Pos).getValue();
-      this.cell1Val = this.cell2Val;
-      sheet.getRange(this.cell1Pos).setValue(this.cell1Val);
-   }
 
-  /* function updateCell2
-   * If cell1 has been updated, change cell2
-   * to reflect
-   */
-   updateCell2(sheet) {
-      this.cell1Val = sheet.getRange(this.cell1Pos).getValue();
-      this.cell2Val = this.cell1Val;
-      sheet.getRange(this.cell2Pos).setValue(this.cell2Val);
-   }
+  checkValues(sheet) {
+    for(var e of this.list) {
+      var newVal = sheet.getRange(e.getA1Notation()).getValue();
+      if( newVal != this.value) {
+        this.value = newVal;
+        setValues(sheet);
+        break;
+      }
+    }
+  }
+
+  setValues(sheet) {
+    for(var e of this.list) {
+      e.setValue(this.value);
+    }
+  }
+
+  toJSON() {
+    var outList = []
+    for(var e of this.list) {
+      outList.push(e.getA1Notation());
+    }
+    return { list: outList, value: this.value };
+  }
+
 }
 
 /* class Quantum
@@ -118,18 +110,37 @@ class Quantum {
     this.set = this.sheet.getRange(en.set)
     this.offset = this.set.offset(0,1);
   }
+/* function makeProtoEntangle
+ * pulls in the list of cells that need to be entangled 
+ * and returns it as an array that can interface with the Entangle
+ * constructor
+ * @return double array of lists of lists of cells
+ */
+  makeProtoEntangle() {
+    var protoList = this.set.getValue();
+    protoList = protoList.split(" ");
+    for(var i = 0; i < protoList.length; i++)
+      protoList[i] = protoList[i].split(",");
+    this.protoList = protoList;
+  }
 }
 /***********************************************************************/
 //triggers
+
 function entangleOpen() {
   if(entangleArray == null)
     return;
   for(var en of entangleArray) {
     var En = new Quantum(en);
-    if (En.offset.getValue() == "") 
-      parseEntangleList(En);
-    else
-      parseEntangleJSON(En);
+    var test = En.offset.getValue();
+    if (En.offset.getValue() == "") {
+      En.makeProtoEntangle();
+      var entangleList = new EntangleList(En.protoList, En.sheet, En.offset);
+      entangleList.refRange.setValue(JSON.stringify(entangleList));
+    }
+    else {
+      entangleRunner(En);
+    }
   }
 }
 
@@ -139,86 +150,20 @@ function entangleEdit() {
     entangleRunner(En);
   }
 }
+
 /***********************************************************************/
-//runner functions
+//helpers
 
 /* function entangleRunner
- * Triggered by an Edit event, gets the JSON version of
- * the Entanglement Array, checks the current state of the
- * Entangled cells. If cell2 updated, it updates cell1 and vice
- * versa
- * @param En - Quantum for current entangle set
+ *
+ * @param En - Quantum 
  */
-function entangleRunner(En) { 
-  var ens = JSON.parse(En.offset.getValue());
-  for(var p of ens) {
-    var check1Val = En.sheet.getRange(p.cell1Pos).getValue()
-    var check2Val = En.sheet.getRange(p.cell2Pos).getValue()
-    if(p.cell1Val != check1Val) {
-      En.sheet.getRange(p.cell2Pos).setValue(check1Val);
-      p.cell1Val = check1Val;
-      p.cell2Val = check1Val;
-      check2Val = check1Val;
-    } 
-    if(p.cell2Val != check2Val) {
-      En.sheet.getRange(p.cell1Pos).setValue(check2Val);
-      p.cell1Val = check2Val;
-      p.cell2Val = check2Val;
-    } 
-  }
-  En.offset.setValue(JSON.stringify(ens));
-}
-
-/***********************************************************************/
-//helper functions
-
-/* function parseEntangleList
- * used to read in the initial entangled pairs
- * cells to entangle are declared at the top of this file
- * by setting the "entangleArray" variable to the cell with
- * pairs constructed in the following format:
- * A1,B1 C2,D2, E5,J6
- * So cell comma cell space cell comma space .....
- * Very important
- * @param Q - Quantum for current sheet
- * @return an array of Entangle objects
- */
-function parseEntangleList(Q) {
-  var en = readArray(Q.set);
-  var ens = [];
-  for(var i = 0; i < en.length; i++) 
-    ens.push(new Entangle(Q.sheet, en[i][0], en[i][1]));
-  Q.offset.setValue(JSON.stringify(ens));
-  return ens;
-}
-
-/* function parseEntangleJSON
- * reads the cell adjacent to entangleArray 
- * parses the content and builds a new Entangle
- * object array 
- * @param Q Quantum used to access the location of the JSON string
- * @return array of Entangle objects
- */
-function parseEntangleJSON(Q) {
-  var ob = JSON.parse(Q.offset.getValue());
-  var ens = [];
-  for(var e of ob) {
-    ens.push(new Entangle(Q.sheet, e.cell1Pos, e.cell2Pos, e.cell1Val, e.cell2Val));
-  }
-  return ens;
-}
-
-/* function readArray
- * pulls in the list of cells that need to be entangled 
- * and returns it as an array that can interface with the Entangle
- * constructor
- * @param range - range to read in
- * @return double array of cells[0-1][cellName]
- */
-function readArray(range) {
-  var i = range.getValue();
-  var i = i.split(" ");
-  for(var j = 0; j < i.length; j++)
-    i[j] = i[j].split(',');
-  return i;
+function entangleRunner(En) {
+  var tempList = JSON.parse(En.offset.getValue());
+  var protoList = []
+  for(var e of tempList.list)
+    protoList.push(e.list);
+  var entangleList = new EntangleList(protoList, En.sheet, En.offset);
+  entangleList.checkListValues();
+  entangleList.refRange.setValue(JSON.stringify(entangleList));
 }
